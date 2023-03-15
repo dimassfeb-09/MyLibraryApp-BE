c77605b536b12c8b57cce3ea95fd8994428c1159
Dimas Febriyanto <dimassfeb@gmail.com>
2023-03-16 06:49:31 +0700

    Route main.go

--- main.go
+++ main.go
@@ -3,9 +3,24 @@ package main
 import (
 	"fmt"
 	"github.com/dimassfeb-09/MyLibraryApp-BE.git/api"
+	"github.com/dimassfeb-09/MyLibraryApp-BE.git/controller"
+	"github.com/dimassfeb-09/MyLibraryApp-BE.git/repository"
+	"github.com/dimassfeb-09/MyLibraryApp-BE.git/service"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	fmt.Println("Hello World")
-	api.DBConn()
+	db := api.DBConn()
+
+	userRepository := repository.NewUserRepositoryImplementation()
+	userService := service.NewUserServiceImplementation(db, userRepository)
+	userController := controller.NewUserControllerImplementation(userService)
+
+	r := gin.New()
+	auth := r.Group("api/v.1/auth")
+	auth.POST("/add", userController.AddUser)
+	auth.GET("/get", userController.GetUserByID)
+
+	r.Run(":8080")
 }
