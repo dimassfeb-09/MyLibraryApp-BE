51f1195cbecba01d2e2c0c5e4a4686c9f3d664c3
Dimas Febriyanto <dimassfeb@gmail.com>
2023-03-16 21:34:36 +0700

    modified: validate required user_controller.go

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"github.com/dimassfeb-09/MyLibraryApp-BE.git/entity/request"
+	"github.com/dimassfeb-09/MyLibraryApp-BE.git/helpers"
 	"github.com/dimassfeb-09/MyLibraryApp-BE.git/service"
 	"github.com/gin-gonic/gin"
 	"log"
@@ -15,7 +16,6 @@ type UserController interface {
 	DeleteUser(c *gin.Context)
 	GetUserByID(c *gin.Context)
 	GetUserByNPM(c *gin.Context)
-	GetUserByEmail(c *gin.Context)
 }
 
 type UserControllerImplementation struct {
@@ -27,80 +27,97 @@ func NewUserControllerImplementation(userService service.UserService) UserContro
 }
 
 func (u *UserControllerImplementation) AddUser(c *gin.Context) {
-	var user request.AddUser
+	var user request.User
 	err := c.ShouldBind(&user)
 	if err != nil {
-		log.Println(err)
-		c.JSON(http.StatusBadRequest, err.Error())
+		c.JSON(http.StatusBadRequest, helpers.ToWebResponse("Bad Request", http.StatusBadRequest, err.Error(), nil))
 		return
 	}
 
 	isSuccess, msg, err := u.UserService.AddUser(c.Request.Context(), &user)
 	if err != nil {
-		log.Println(err)
-		c.JSON(http.StatusBadRequest, map[string]any{
-			"status": "Bad Request",
-			"code":   http.StatusBadRequest,
-			"msg":    msg,
-			"data":   nil,
-		})
+		c.JSON(http.StatusBadRequest, helpers.ToWebResponse("Bad Request", http.StatusBadRequest, msg, nil))
 		return
 	}
 
 	if isSuccess {
-		c.JSON(http.StatusOK, map[string]any{
-			"status": "OK",
-			"code":   http.StatusOK,
-			"msg":    msg,
-			"data":   nil,
-		})
+		c.JSON(http.StatusBadRequest, helpers.ToWebResponse("OK", http.StatusOK, msg, nil))
 		return
 	}
 }
 
 func (u *UserControllerImplementation) UpdateUser(c *gin.Context) {
-	// TODO
+	var user request.User
+	err := c.ShouldBind(&user)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusBadRequest, helpers.ToWebResponse("Bad Request", http.StatusBadRequest, err.Error(), nil))
+		return
+	}
+
+	isSuccess, msg, err := u.UserService.UpdateUser(c.Request.Context(), &user)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, helpers.ToWebResponse("Bad Request", http.StatusBadRequest, msg, nil))
+		return
+	}
+
+	if isSuccess {
+		c.JSON(http.StatusBadRequest, helpers.ToWebResponse("OK", http.StatusOK, msg, nil))
+		return
+	}
 }
 
 func (u *UserControllerImplementation) DeleteUser(c *gin.Context) {
-	//TODO implement me
-	panic("implement me")
+	var user struct {
+		ID int `json:"id"`
+	}
+
+	err := c.ShouldBind(&user)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, helpers.ToWebResponse("Bad Request", http.StatusBadRequest, "Invalid ID", nil))
+		return
+	}
+
+	isSuccess, msg, err := u.UserService.DeleteUser(c.Request.Context(), user.ID)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, helpers.ToWebResponse("Bad Request", http.StatusBadRequest, msg, nil))
+		return
+	}
+
+	if isSuccess {
+		c.JSON(http.StatusOK, helpers.ToWebResponse("OK", http.StatusOK, msg, nil))
+		return
+	}
+
 }
 
 func (u *UserControllerImplementation) GetUserByID(c *gin.Context) {
 	ID, err := strconv.Atoi(c.Query("id"))
 	if err != nil {
-		c.AbortWithStatusJSON(http.StatusBadRequest, "Invalid ID")
+		c.AbortWithStatusJSON(http.StatusBadRequest, helpers.ToWebResponse("Bad Request", http.StatusBadRequest, "Invalid ID", nil))
 		return
 	}
 
 	userByID, msg, err := u.UserService.GetUserByID(c.Request.Context(), ID)
 	if err != nil {
-		c.AbortWithStatusJSON(http.StatusBadRequest, map[string]any{
-			"status": "Bad Request",
-			"code":   http.StatusBadRequest,
-			"msg":    msg,
-			"data":   nil,
-		})
+		c.AbortWithStatusJSON(http.StatusBadRequest, helpers.ToWebResponse("Bad Request", http.StatusBadRequest, msg, nil))
 		return
 	}
 	if userByID != nil {
-		c.JSON(http.StatusOK, map[string]any{
-			"status": "OK",
-			"code":   http.StatusOK,
-			"msg":    msg,
-			"data":   userByID,
-		})
+		c.JSON(http.StatusOK, helpers.ToWebResponse("OK", http.StatusOK, msg, userByID))
 		return
 	}
 }
 
 func (u *UserControllerImplementation) GetUserByNPM(c *gin.Context) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (u *UserControllerImplementation) GetUserByEmail(c *gin.Context) {
-	//TODO implement me
-	panic("implement me")
+	NPM := c.Query("npm")
+	userByNPM, msg, err := u.UserService.GetUserByNPM(c.Request.Context(), NPM)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, helpers.ToWebResponse("Bad Request", http.StatusBadRequest, msg, nil))
+		return
+	}
+	if userByNPM != nil {
+		c.JSON(http.StatusOK, helpers.ToWebResponse("OK", http.StatusOK, msg, userByNPM))
+		return
+	}
 }
