081f757bf61eef71895d1217b2010d919e855a03
Dimas Febriyanto <dimassfeb@gmail.com>
2023-03-17 20:49:36 +0700

    modified: api/rooute.go adding category route

--- api/routes.go
+++ api/routes.go
@@ -9,6 +9,10 @@ import (
 )
 
 func GinRoute(db *gorm.DB) *gin.Engine {
+
+	r := gin.New()
+	r.HandleMethodNotAllowed = true
+
 	userRepository := repository.NewUserRepositoryImplementation()
 	userService := service.NewUserServiceImplementation(db, userRepository)
 	userController := controller.NewUserControllerImplementation(userService)
@@ -17,8 +21,9 @@ func GinRoute(db *gorm.DB) *gin.Engine {
 	authService := service.NewAuthServiceImplementation(db, authRepository, userService)
 	authController := controller.NewAuthControllerImplementation(authService)
 
-	r := gin.New()
-	r.HandleMethodNotAllowed = true
+	categoryRepository := repository.NewCategoryRepositoryImplementation()
+	categoryService := service.NewCategoryServiceImplementation(db, categoryRepository)
+	categoryController := controller.NewCategoryControllerImplementation(categoryService)
 
 	api := r.Group("api/v.1")
 	user := api.Group("/user")
@@ -28,14 +33,34 @@ func GinRoute(db *gorm.DB) *gin.Engine {
 	user.GET("/get", func(c *gin.Context) {
 		if c.Query("id") != "" {
 			userController.GetUserByID(c)
+			return
 		} else if c.Query("npm") != "" {
 			userController.GetUserByNPM(c)
+			return
 		}
 	})
 
 	auth := api.Group("/auth")
 	auth.POST("/register", authController.AuthRegister)
 	auth.POST("/login", authController.AuthLogin)
+
+	category := api.Group("/category")
+	category.POST("/add", categoryController.AddCategory)
+	category.PUT("/update", categoryController.UpdateCategory)
+	category.DELETE("/delete", categoryController.DeleteCategory)
+	category.GET("/get", func(c *gin.Context) {
+		if c.Query("id") != "" {
+			categoryController.GetCategoryByID(c)
+			return
+		} else if c.Query("name") != "" {
+			categoryController.GetCategoryByName(c)
+			return
+		} else {
+			categoryController.GetCategories(c)
+			return
+		}
+
+	})
 
 	return r
 }
