d390462d46b43667860a28bdcad87c5fcc21c207
Dimas Febriyanto <dimassfeb@gmail.com>
2023-03-17 18:56:35 +0700

    modified auth_service, api/database

--- api/database.go
+++ api/database.go
@@ -1,20 +1,18 @@
 package api
 
 import (
-	"log"
-
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-func DBConn() *gorm.DB {
-	dsn := "root:root@tcp(127.0.0.1:3306)/mylibraryapp?charset=utf8mb4&parseTime=True&loc=Local"
+func DBConn() (*gorm.DB, error) {
+	dsn := "root:root@tcp(127.0.0.1:3306)/mylibraryapps?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
 		SkipDefaultTransaction: true,
 	})
 	if err != nil {
-		log.Println(err)
+		return nil, err
 	}
 
-	return db.Session(&gorm.Session{SkipDefaultTransaction: true})
+	return db.Session(&gorm.Session{SkipDefaultTransaction: true}), nil
 }
---
+++ service/auth_service.go
@@ -0,0 +1,76 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/dimassfeb-09/MyLibraryApp-BE.git/entity/domain"
+	"github.com/dimassfeb-09/MyLibraryApp-BE.git/entity/request"
+	"github.com/dimassfeb-09/MyLibraryApp-BE.git/repository"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
+
+type AuthService interface {
+	AuthRegister(ctx context.Context, register *request.AuthRegister) (isSuccess bool, msg string, err error)
+	AuthLogin(ctx context.Context, login *request.AuthLogin) (isSuccess bool, msg string, err error)
+}
+
+type AuthServiceImplementation struct {
+	DB             *gorm.DB
+	AuthRepository repository.AuthRepository
+	UserService    UserService
+}
+
+func NewAuthServiceImplementation(DB *gorm.DB, authRepository repository.AuthRepository, userService UserService) AuthService {
+	return &AuthServiceImplementation{DB: DB, AuthRepository: authRepository, UserService: userService}
+}
+
+func (a *AuthServiceImplementation) AuthRegister(ctx context.Context, r *request.AuthRegister) (bool, string, error) {
+
+	_, msg, err := a.UserService.GetUserByEmail(ctx, r.Email)
+	if err != gorm.ErrRecordNotFound {
+		return false, "Email telah digunakan.", gorm.ErrRegistered
+	}
+
+	_, msg, err = a.UserService.GetUserByNPM(ctx, r.NPM)
+	if err != gorm.ErrRecordNotFound {
+		return false, "NPM telah digunakan.", gorm.ErrRegistered
+	}
+
+	hashPassword, err := bcrypt.GenerateFromPassword([]byte(r.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return false, "Internal Server Error", errors.New("Internal Server Error")
+	}
+
+	fmt.Println(string(hashPassword))
+
+	authRegister := &domain.AuthRegister{
+		Name:     r.Name,
+		NPM:      r.NPM,
+		Email:    r.Email,
+		Password: string(hashPassword),
+		IsGoogle: r.IsGoogle,
+	}
+	_, msg, err = a.AuthRepository.AuthRegister(ctx, a.DB, authRegister)
+	if err != nil {
+		return false, msg, err
+	}
+
+	return false, msg, err
+}
+
+func (a *AuthServiceImplementation) AuthLogin(ctx context.Context, login *request.AuthLogin) (bool, string, error) {
+
+	loginDetail, err := a.AuthRepository.AuthLogin(ctx, a.DB, login.Email)
+	if err != nil {
+		return false, "Internal Server Error.", err
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(loginDetail.Password), []byte(login.Password))
+	if err != nil {
+		return false, "Password salah, silahkan cek kembali.", err
+	}
+
+	return true, "Berhasil Login.", nil
+}
