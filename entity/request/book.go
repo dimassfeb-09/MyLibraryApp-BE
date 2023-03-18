a9c51d9288334dc95ad146b5ea4a0d7407cefb4b
Dimas Febriyanto <dimassfeb@gmail.com>
2023-03-18 23:10:58 +0700

    add entitiy for book

---
+++ entity/request/book.go
@@ -0,0 +1,12 @@
+package request
+
+type Book struct {
+	ID          int     `json:"id"`
+	Title       string  `binding:"required" json:"title"`
+	Description string  `binding:"required" json:"description"`
+	Stok        int     `binding:"required" json:"stok"`
+	Writer      string  `binding:"required" json:"writer"`
+	ImgURL      string  `binding:"required" json:"img_url"`
+	Rating      float32 `binding:"required" json:"rating"`
+	CategoryID  int     `binding:"required" json:"category_id"`
+}
